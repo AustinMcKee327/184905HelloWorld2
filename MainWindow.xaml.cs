b3d1d0f5de30048ff82842f0970e7d9fbfc56f17
AustinMcKee327 <47399911+AustinMcKee327@users.noreply.github.com>
2019-02-08 13:14:52 -0500

    Added files to start working on it at h

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿/* Austin McKee
+ * Feb 6, 2019
+ * Hello World my first Program, understand most of it, help with understanding .cs?
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184905HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            String name = txtName.Text;
+            message.Content = "Hello, " + name;
+        }
+    }
+}
